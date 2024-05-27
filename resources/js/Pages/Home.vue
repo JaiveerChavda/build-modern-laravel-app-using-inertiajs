9ca995e54281fc6fb3bc7d7db3d8f7ecf9e63ec3
jaiveer-truptman <jchavda@truptman.in>
2024-05-27 17:24:13 +0530

    setting up client adapter for vue and made first page containing data

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,16 @@
+<script>
+export default{
+    props : {
+        name: String,
+        frameworks: Array,
+    }
+}
+</script>
+<template>
+    <div class="card">
+        <h1>{{ name }}</h1>
+        <ul>
+            <li v-for="framework of frameworks" v-text="framework"></li>
+        </ul>
+    </div>
+</template>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,11 @@
+<script>
+export default{
+    props: {
+        name: String,
+    }
+}
+</script>
+<template>
+    <h1>Hello world</h1>
+    <p>hello , i am {{ name }}</p>
+</template>
